54435e55df6e0cd1519a16f6622f959aed709100
AlmazBag <almazbagmanov@yandex.ru>
2025-09-17 13:05:55 +0300

    Обработка исключений с помощью exceptionHandler

---
+++ src/main/kotlin/exceptions/Test.kt
@@ -0,0 +1,45 @@
+package exceptions
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val exceptionHandler = CoroutineExceptionHandler { context, throwable ->
+    println("Catched exception ${throwable.message}")
+}
+private val scope = CoroutineScope(dispatcher + exceptionHandler)
+
+fun main() {
+
+    scope.launch {
+        method()
+//            runCatching {
+//                method()
+//            }.onSuccess {
+//                println("success")
+//            }.onFailure {
+//                println("failure")
+//            }
+
+    }
+
+    scope.launch {
+        method2()
+    }
+
+}
+
+suspend fun method(): String {
+    delay(3000)
+    error("Some error 1")
+}
+
+suspend fun method2()   {
+    delay(5000)
+    println("Some error 2")
+}
